0413e70508702378a93b267ead3a98bffed0ffce
gabriel <gabesoft@gmail.com>
2011-05-30 20:50:23 -0700

    mongo connection

--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,8 @@ module ShopGuide
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
+    config.gem "mongo_mapper"
+
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
   end
--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,8 +1,15 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "shop-guide-db-#{Rails.env}"
-
-if defined? PhusionPassenger
-  PhusionPassenger.on_event :starting_worker_process do |forked|
-    MongoMapper.connection.connect if forked
-  end 
+if ENV['MONGOHQ_URL']
+  MongoMapper.config = { RAILS_ENV => { 'uri' => ENV['MONGOHQ_URL'] } }
+else 
+  MongoMapper.config = { RAILS_ENV => { 'uri' => 'mongodb://localhost:27017' } }
 end
+
+
+#MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+#MongoMapper.database = "shop-guide-db-#{Rails.env}"
+
+#if defined? PhusionPassenger
+  #PhusionPassenger.on_event :starting_worker_process do |forked|
+    #MongoMapper.connection.connect if forked
+  #end 
+#end
