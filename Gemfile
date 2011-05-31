07d623a5873cbf3a5e9d357a7bbac1f7bcad0dce
gabriel <gabesoft@gmail.com>
2011-05-30 19:50:42 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+source 'http://gemcutter.org'
+
+require 'rubygems'
+require 'mongo'
+
+gem 'rails', '3.0.7'
+gem 'mongo_mapper'
+gem 'bson'
+gem 'bson_ext'
+gem 'haml'
+gem 'json'
+
+
+group :development do
+  gem 'thin'
+end
+
+group :test do
+  gem 'rspec'
+  gem 'webrat'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShopGuide::Application.config.secret_token = '2f6386c3cae6aabe8eb866c100d7ef9af8393ece7172af12e7beb7c60981249e681f12c5585dbab5eff76c0e163478cc8019e048a827c74db70e63703a08c8bc'
