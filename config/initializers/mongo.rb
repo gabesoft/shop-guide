cd07ce692bbde5b65fe51653fabf1868b212cc43
gabriel <gabesoft@gmail.com>
2011-05-31 22:24:22 -0700

    mongo connection update

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,16 +1,9 @@
+db_name = 'shop-guide-db'
+
 if ENV['MONGOHQ_URL']
   MongoMapper.config = { RAILS_ENV => { 'uri' => ENV['MONGOHQ_URL'] } }
 else 
-  MongoMapper.config = { RAILS_ENV => { 'uri' => 'mongodb://localhost:27017' } }
+  MongoMapper.config = { RAILS_ENV => { 'uri' => "mongodb://localhost:27017/#{db_name}-#{Rails.env}" } }
 end
 
-db_name = 'shop-guide-db'
-MongoMapper.database = "#{db_name}-#{Rails.env}"
-
-#MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-
-#if defined? PhusionPassenger
-  #PhusionPassenger.on_event :starting_worker_process do |forked|
-    #MongoMapper.connection.connect if forked
-  #end 
-#end
+MongoMapper.connect(RAILS_ENV)
