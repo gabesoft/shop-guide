4098333b83bc13057bacf25d6bb84d48895971b5
gabriel <gabesoft@gmail.com>
2011-06-03 17:11:24 -0700

    ext js dependencies

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,22 +1,10 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+Ext.Loader.setConfig({enabled: true});
+Ext.Loader.setPath({'AppName': 'app'});
+Ext.Loader.setPath({'Ext': 'javascripts/lib/extjs/src' });
 
-//$('a[data-remote=true]').livequery('click', function() {
-  //$.ajax({
-    //url: this.href,
-  //dataType: "script"
-  //});
-  //return false;
-//});
-
-
-//$('form[data-remote=true]').livequery('submit', function() {
-  //return request({ url : this.action, type : this.method, data : $(this).serialize() });
-//});
-
-//$(function() {
-  //$(".alert").click(function() {
-    //alert(this.getAttribute("data-confirm"));
-    //return false;
-  //})
-//})
+Ext.onReady(function () {
+  Ext.create('Ext.Component', {
+    renderTo: 'container',
+    html: 'Ready to start coding'
+  });
+});
